54068e356c978333ce1d0c4827a78447fa69c7d5
milich97 <misha199764@gmail.com>
2018-04-12 23:27:20 +0300

    information page added

--- src/main/java/system/controller/DeliveryController.java
+++ src/main/java/system/controller/DeliveryController.java
@@ -31,6 +31,11 @@ public class DeliveryController {
         return "/login.html";
     }
 
+    @RequestMapping(value = "/information", method = RequestMethod.GET)
+    public String information() {
+        return "/information.html";
+    }
+
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration() {
         return "/registration.html";
